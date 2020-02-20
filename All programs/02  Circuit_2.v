64abdbf8a57c55562cba2314abbe3d6a0c290716
Yifan Zhu <zhuyi@tcd.ie>
2020-02-20 20:44:16 +0000

    Create 02  Circuit_2.v

---
+++ All programs/02  Circuit_2.v
@@ -0,0 +1,8 @@
+module	Circuit_2 (Out_1, Out_2, Out_3, A, B, C, D);
+output	Out_1, Out_2, Out_3;
+input   A, B, C, D;
+
+assign	Out_1 = (A || (!B)) && (!C) && (C || D);
+assign	Out_2 = ((!C) && D || (B && C && D) || (C && (!D))) && ((!A) || B);
+assign	Out_3 = (A && B || C) && D || (!B) && C;
+endmodule
